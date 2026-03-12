316b5f74ac7d08e2eb9898825b0de84b3c82d68e
Matthew King <matthewbrking@gmail.com>
2026-03-12 18:28:55 -0400

    Add project files.

---
+++ Core/IInstalllOption.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsolifyCLI.Core
+{
+    public interface IInstallOption
+    {
+        int Id { get; }
+        string Name { get; }
+        bool IsSelected { get; set; }
+
+        Task ExecuteAsync();
+    }
+}
